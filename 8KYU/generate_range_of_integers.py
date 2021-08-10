1ddd61b9ac84d0695c1e899e36c1fcf71b93c8a1
Robin <robinwettstadt@gmail.com>
2021-08-10 19:45:14 +0200

    starting with some 8KYU problems

---
+++ 8KYU/generate_range_of_integers.py
@@ -0,0 +1,8 @@
+def generate_range(min, max, step):
+    
+    list = []
+    
+    for i in range(min, max+1, step):
+        list.append(i)
+    
+    return list
---
+++ 8KYU/is_it_a_palindrome.py
@@ -0,0 +1,2 @@
+def is_palindrome(s):
+    return True if s.lower() == s[::-1].lower() else False
